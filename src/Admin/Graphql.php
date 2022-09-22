efb1e3be1022214a7e77ee432b49397cfb2c773c
Aimeos <aimeos@aimeos.org>
2022-09-22 12:18:23 +0200

    Pass PSR-7 object around

--- src/Admin/Graphql.php
+++ src/Admin/Graphql.php
@@ -6,57 +6,73 @@ namespace Aimeos\Admin;
 use GraphQL\GraphQL;
 use GraphQL\Type\Schema;
 use GraphQL\Type\Definition\ObjectType;
+use Nyholm\Psr7\Factory\Psr17Factory;
 
 
 class Graphql
 {
-	public static function execute( \Aimeos\MShop\ContextIface $context ) : string
-    {
-        try
-        {
-            $request = $context->view()->request();
-            $input = json_decode( $request->getBody(), true);
+	/**
+	 * Executes the GraphQL request
+	 *
+	 * @param \Aimeos\MShop\ContextIface $context Context object
+	 * @param \Psr\Http\Message\ServerRequestInterface $request Request object
+	 * @return \Psr\Http\Message\ResponseInterface Response object containing the generated output
+	 */
+	public static function execute( \Aimeos\MShop\ContextIface $context,
+		\Psr\Http\Message\ServerRequestInterface $request ) : \Psr\Http\Message\ResponseInterface
+	{
+		try
+		{
+			$input = json_decode( (string) $request->getBody(), true);
 
-            return GraphQL::executeQuery(
-                self::schema( $context ),
-                $input['query'] ?? null,
-                [], // root
-                null, // context
-                $input['variables'] ?? null,
-                $input['operationName'] ?? null
-            )->toArray();
-        }
-        catch( \Throwable $t )
-        {
-            return [
-                'errors' => [[
-                    'message' => $t->getMessage()
-                ]]
-            ];
-        }
-    }
+			$result = GraphQL::executeQuery(
+				self::schema( $context ),
+				$input['query'] ?? null,
+				[], // root
+				null, // context
+				$input['variables'] ?? null,
+				$input['operationName'] ?? null
+			)->toArray();
+		}
+		catch( \Throwable $t )
+		{
+			$result = [
+				'errors' => [[
+					'message' => $t->getMessage()
+				]]
+			];
+		}
+
+		return ( new Psr17Factory )->createResponse()->withBody( json_encode( $result ) );
+	}
 
 
-    protected static function schema( \Aimeos\MShop\ContextIface $context ) : array
-    {
-        $types = [];
-        $domains = $context->config()->get( 'admin/graphql/domains', ['product'] );
+	/**
+	 * Returns the GraphQL schema
+	 *
+	 * @param \Aimeos\MShop\ContextIface $context Context object
+	 * @return array List of GraphQL query schema definitions
+	 */
+	protected static function schema( \Aimeos\MShop\ContextIface $context ) : array
+	{
+		$types = [];
+		$domains = $context->config()->get( 'admin/graphql/domains', ['product'] );
 
-        foreach( $domains as $domain )
-        {
-            $name = $context->config()->get( 'admin/graphql/' . $domain . '/name', 'Standard' );
+		foreach( $domains as $domain )
+		{
+			$name = $context->config()->get( 'admin/graphql/' . $domain . '/name', 'Standard' );
 
-            $classname = '\Aimeos\Graphql\\' . \ucfirst( $domain ) . '\\Standard';
-            $object = new $classname( $context );
+			$classname = '\Aimeos\Graphql\\' . ucfirst( $domain ) . '\\Standard';
+			$object = new $classname( $context );
 
-            $types[$domain] = $object->type();
-        }
+			$types[$domain] = $object->type();
+		}
 
-        return new Schema([
-            'query' => new ObjectType( [
-                'name' => 'query',
-                'fields' => $types
-            ] ),
-        ] );
-    }
+		return new Schema([
+			'query' => new ObjectType( [
+				'name' => 'query',
+				'fields' => $types
+			] ),
+		] );
+	}
 }
