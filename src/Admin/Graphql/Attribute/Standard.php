39f1f0875e4747632db7ad686a723c91ef81fd91
Aimeos <aimeos@aimeos.org>
2022-10-16 18:21:48 +0200

    Added find() methods for attribute/catalog/customer/service/supplier domains

---
+++ src/Admin/Graphql/Attribute/Standard.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2022
+ * @package Admin
+ * @subpackage GraphQL
+ */
+
+
+namespace Aimeos\Admin\Graphql\Attribute;
+
+use GraphQL\Type\Definition\Type;
+
+
+/**
+ * GraphQL class for special handling of attributes
+ *
+ * @package Admin
+ * @subpackage GraphQL
+ */
+class Standard extends \Aimeos\Admin\Graphql\Standard
+{
+	/**
+	 * Returns GraphQL schema definition for the available queries
+	 *
+	 * @param string $domain Domain name of the responsible manager
+	 * @return array GraphQL query schema definition
+	 */
+	public function query( string $domain ) : array
+	{
+		$list = parent::query( $domain );
+
+		$list['findAttribute'] = [
+			'type' => $this->outputType( $domain ),
+			'args' => [
+				['name' => 'code', 'type' => Type::string(), 'description' => 'Unique code'],
+				['name' => 'domain', 'type' => Type::string(), 'description' => 'Domain of the attribute'],
+				['name' => 'type', 'type' => Type::string(), 'description' => 'Attribute type'],
+				['name' => 'include', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Domains to include'],
+			],
+			'resolve' => $this->findItem( $domain ),
+		];
+
+		return $list;
+	}
+
+
+	/**
+	 * Returns a closure for returning a single item by its code
+	 *
+	 * @param string $domain Domain path of the manager
+	 * @return \Closure Anonymous method returning one item
+	 */
+	protected function findItem( string $domain ) : \Closure
+	{
+		return function( $root, $args, $context ) use ( $domain ) {
+			return \Aimeos\MShop::create( $this->context(), $domain )->find( $args['code'], $args['include'], $args['domain'], $args['type'] );
+		};
+	}
+}
