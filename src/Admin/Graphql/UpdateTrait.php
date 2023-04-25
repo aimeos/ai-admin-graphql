c2c519613eec5cef775e000fd15cdcb371c148a8
Tobias Jern <tobias.jern@aalto.fi>
2023-04-25 10:16:20 +0300

    Avoid creating refItem if not referenced
    Check if subentry item is defined,
    and avoid craeting the refItem if it is not referenced.
    
    Was otherwise causing it to create "empty" items
    when the referenced item was not passed.

--- src/Admin/Graphql/UpdateTrait.php
+++ src/Admin/Graphql/UpdateTrait.php
@@ -90,15 +90,18 @@ trait UpdateTrait
 			foreach( $list as $subentry )
 			{
 				$id = $subentry['item'][$domain.'.id'] ?? '';
-				
+
 				$listItem = $listItems->find( function( $item ) use ( $id ) {
 				    return $id == $item->getRefId();
 				}, $manager->createListItem() );
-				
+
 				unset( $listItems[$listItem->getId()] );
 
-				$refItem = $listItem->getRefItem() ?: $domainManager->create();
-				$refItem->fromArray( $subentry['item'] ?? [], true );
+				$refItem = null;
+				if ( isset( $subentry['item'] ) ) {
+				    $refItem = $listItem->getRefItem() ?: $domainManager->create();
+				    $refItem->fromArray( $subentry['item'], true );
+				}
 
 				if( isset( $subentry['item']['address'] ) && $refItem instanceof \Aimeos\MShop\Common\Item\AddressRef\Iface ) {
 					$refItem = $this->updateAddresses( $domainManager, $refItem, $subentry['item']['address'] );
