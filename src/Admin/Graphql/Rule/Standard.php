4e0e86798bef09f22ff892e401e414fae4267b8a
Aimeos <aimeos@aimeos.org>
2023-06-06 11:55:08 +0200

    Added query for retrieving rule backend config

---
+++ src/Admin/Graphql/Rule/Standard.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2023
+ * @package Admin
+ * @subpackage GraphQL
+ */
+
+
+namespace Aimeos\Admin\Graphql\Rule;
+
+
+/**
+ * GraphQL class for special handling of plugins
+ *
+ * @package Admin
+ * @subpackage GraphQL
+ */
+class Standard extends \Aimeos\Admin\Graphql\Standard
+{
+	use \Aimeos\Admin\Graphql\ProviderTrait;
+}
---
+++ tests/Admin/Rule/StandardTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2023
+ */
+
+
+namespace Aimeos\Admin\Rule;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $context;
+
+
+	protected function setUp() : void
+	{
+		\Aimeos\MShop::cache( true );
+		$this->context = \TestHelper::context();
+		$this->context->config()->set( 'admin/graphql/debug', true );
+		$this->context->setView( \TestHelper::view( 'unittest', $this->context->config() ) );
+	}
+
+
+	public function testGetRuleConfig()
+	{
+		$body = '{"query":"query {\n  getRuleConfig(provider: \"Percent,Category\", type: \"catalog\") {\n    code\n    type\n    label\n  }\n}\n","variables":{},"operationName":null}';
+		$request = new \Nyholm\Psr7\ServerRequest( 'POST', 'localhost', [], $body );
+
+		$response = \Aimeos\Admin\Graphql::execute( $this->context, $request );
+
+		$this->assertStringContainsString( '"code":"percent"', (string) $response->getBody() );
+		$this->assertStringContainsString( '"code":"category.code"', (string) $response->getBody() );
+	}
+}
