e2aba892e7720cc4c1a4507bad549eee3fe66400
Aimeos <aimeos@aimeos.org>
2024-05-21 14:22:58 +0200

    Implemented retrieving groups along with customer items

--- src/Admin/Graphql/Customer/Standard.php
+++ src/Admin/Graphql/Customer/Standard.php
@@ -11,6 +11,9 @@
 namespace Aimeos\Admin\Graphql\Customer;
 
 use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\ResolveInfo;
+use Aimeos\MShop\Common\Item\Iface as ItemIface;
 
 
 /**
@@ -21,6 +24,9 @@ use GraphQL\Type\Definition\Type;
  */
 class Standard extends \Aimeos\Admin\Graphql\Standard
 {
+	private $type;
+
+
 	/**
 	 * Returns GraphQL schema definition for the available queries
 	 *
@@ -29,31 +35,115 @@ class Standard extends \Aimeos\Admin\Graphql\Standard
 	 */
 	public function query( string $domain ) : array
 	{
-		$list = parent::query( $domain );
-
-		$list['findCustomer'] = [
-			'type' => $this->types()->outputType( $domain ),
-			'args' => [
-				['name' => 'code', 'type' => Type::string(), 'description' => 'Unique code'],
-				['name' => 'include', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Domains to include'],
+		$list = [
+			'aggregateCustomers' => [
+				'type' => $this->types()->aggregateOutputType( $domain ),
+				'args' => [
+					['name' => 'key', 'type' => Type::listOf( Type::string() ), 'description' => 'Aggregation key to group results by, e.g. "customer.status"'],
+					['name' => 'value', 'type' => Type::string(), 'defaultValue' => null, 'description' => 'Aggregate values from that column, e.g "customer.status" (optional, only if type is passed)'],
+					['name' => 'type', 'type' => Type::string(), 'defaultValue' => null, 'description' => 'Type of aggregation like "sum" or "avg" (default: null for count)'],
+					['name' => 'filter', 'type' => Type::string(), 'defaultValue' => '{}', 'description' => 'Filter conditions'],
+					['name' => 'sort', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Sort keys'],
+					['name' => 'limit', 'type' => Type::int(), 'defaultValue' => 10000, 'description' => 'Slice size'],
+				],
+				'resolve' => $this->aggregateItems( $domain ),
 			],
-			'resolve' => $this->findItem( $domain ),
-		];
-
-		$list['aggregateCustomers'] = [
-			'type' => $this->types()->aggregateOutputType( $domain ),
-			'args' => [
-				['name' => 'key', 'type' => Type::listOf( Type::string() ), 'description' => 'Aggregation key to group results by, e.g. "customer.status"'],
-				['name' => 'value', 'type' => Type::string(), 'defaultValue' => null, 'description' => 'Aggregate values from that column, e.g "customer.status" (optional, only if type is passed)'],
-				['name' => 'type', 'type' => Type::string(), 'defaultValue' => null, 'description' => 'Type of aggregation like "sum" or "avg" (default: null for count)'],
-				['name' => 'filter', 'type' => Type::string(), 'defaultValue' => '{}', 'description' => 'Filter conditions'],
-				['name' => 'sort', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Sort keys'],
-				['name' => 'limit', 'type' => Type::int(), 'defaultValue' => 10000, 'description' => 'Slice size'],
+			'getCustomer' => [
+				'type' => $this->outputType( $domain ),
+				'args' => [
+					['name' => 'id', 'type' => Type::string(), 'description' => 'Unique ID'],
+					['name' => 'include', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Domains to include'],
+				],
+				'resolve' => $this->getItem( $domain ),
 			],
-			'resolve' => $this->aggregateItems( $domain ),
+			'findCustomer' => [
+				'type' => $this->outputType( $domain ),
+				'args' => [
+					['name' => 'code', 'type' => Type::string(), 'description' => 'Unique code'],
+					['name' => 'include', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Domains to include'],
+				],
+				'resolve' => $this->findItem( $domain ),
+			],
+			'searchCustomers' => [
+				'type' => $this->types()->searchOutputType( $domain, fn( $path ) => $this->outputType( $path ) ),
+				'args' => [
+					['name' => 'filter', 'type' => Type::string(), 'defaultValue' => '{}', 'description' => 'Filter conditions'],
+					['name' => 'include', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Domains to include'],
+					['name' => 'sort', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Sort keys'],
+					['name' => 'offset', 'type' => Type::int(), 'defaultValue' => 0, 'description' => 'Slice offset'],
+					['name' => 'limit', 'type' => Type::int(), 'defaultValue' => 100, 'description' => 'Slice size'],
+				],
+				'resolve' => $this->searchItems( $domain ),
+			]
 		];
 
 		return $list;
+	}
+
+
+	/**
+	 * Defines the GraphQL output types
+	 *
+	 * @param string $path Path of the domain manager
+	 * @return \GraphQL\Type\Definition\ObjectType Output type definition
+	 */
+	public function outputType( string $path ) : ObjectType
+	{
+		$name = str_replace( '/', '', ucwords( $path, '/' ) ) . 'Output';
+
+		if( isset( $this->type ) ) {
+			return $this->type;
+		}
+
+		return $this->type = new ObjectType( [
+			'name' => $name,
+			'fields' => function() use ( $path ) {
+
+				$manager = \Aimeos\MShop::create( $this->context(), $path );
+				$item = $manager->create();
+
+				$list = $this->types()->fields( $manager->getSearchAttributes( false ) );
+				$list['groups'] = [
+					'type' => Type::listOf( Type::String() ),
+					'description' => 'List of group IDs assigned to the account',
+					'resolve' => function( $item, $args ) {
+						return $item->getGroups();
+					}
+				];
+
+				if( $item instanceof \Aimeos\MShop\Common\Item\AddressRef\Iface ) {
+					$list['address'] = Type::listOf( $this->types()->addressOutputType( $path . '/address' ) );
+				}
+
+				if( $item instanceof \Aimeos\MShop\Common\Item\PropertyRef\Iface )
+				{
+					$list['property'] = [
+						'type' => Type::listOf( $this->types()->propertyOutputType( $path . '/property' ) ),
+						'args' => [
+							'type' => Type::listOf( Type::String() ),
+						],
+						'resolve' => function( $item, $args ) {
+							return $item->getPropertyItems( $args['type'] ?? null, false );
+						}
+					];
+				}
+
+				if( $item instanceof \Aimeos\MShop\Common\Item\ListsRef\Iface )
+				{
+					$list['lists'] = [
+						'type' => $this->types()->listsOutputType( $path . '/lists' ),
+						'resolve' => function( ItemIface $item, array $args ) {
+							return $item;
+						}
+					];
+				}
+
+				return $list;
+			},
+			'resolveField' => function( ItemIface $item, array $args, $context, ResolveInfo $info ) use ( $path ) {
+				return $this->types()->resolve( $item, $path, $info->fieldName );
+			}
+		] );
 	}
 
 
