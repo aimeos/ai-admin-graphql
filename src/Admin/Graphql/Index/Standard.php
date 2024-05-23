6fb7cf7f8c2f749b629635d36f229d6b6d6d3125
Aimeos <aimeos@aimeos.org>
2024-05-23 20:42:02 +0200

    Avoid mixing index with product output type

--- src/Admin/Graphql/Index/Standard.php
+++ src/Admin/Graphql/Index/Standard.php
@@ -22,6 +22,18 @@ use GraphQL\Type\Definition\Type;
 class Standard extends \Aimeos\Admin\Graphql\Standard
 {
 	/**
+	 * Returns GraphQL schema definition for the available mutations
+	 *
+	 * @param string $domain Domain name of the responsible manager
+	 * @return array GraphQL mutation schema definition
+	 */
+	public function mutation( string $domain ) : array
+	{
+		return [];
+	}
+
+
+	/**
 	 * Returns GraphQL schema definition for the available queries
 	 *
 	 * @param string $domain Domain name of the responsible manager
@@ -29,21 +41,19 @@ class Standard extends \Aimeos\Admin\Graphql\Standard
 	 */
 	public function query( string $domain ) : array
 	{
-		$list = parent::query( $domain );
-
-		$list['aggregateIndex'] = [
-			'type' => $this->types()->aggregateOutputType( $domain ),
-			'args' => [
-				['name' => 'key', 'type' => Type::listOf( Type::string() ), 'description' => 'Aggregation key to group results by, e.g. ["product.status", "index.catalog.id"]'],
-				['name' => 'value', 'type' => Type::string(), 'defaultValue' => null, 'description' => 'Aggregate values from that column, e.g "index.catalog.id" (optional, only if type is passed)'],
-				['name' => 'type', 'type' => Type::string(), 'defaultValue' => null, 'description' => 'Type of aggregation like "sum" or "avg" (default: null for count)'],
-				['name' => 'filter', 'type' => Type::string(), 'defaultValue' => '{}', 'description' => 'Filter conditions'],
-				['name' => 'sort', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Sort keys'],
-				['name' => 'limit', 'type' => Type::int(), 'defaultValue' => 10000, 'description' => 'Slice size'],
-			],
-			'resolve' => $this->aggregateItems( $domain ),
+		return [
+			'aggregateIndex' => [
+				'type' => $this->types()->aggregateOutputType( $domain ),
+				'args' => [
+					['name' => 'key', 'type' => Type::listOf( Type::string() ), 'description' => 'Aggregation key to group results by, e.g. ["product.status", "index.catalog.id"]'],
+					['name' => 'value', 'type' => Type::string(), 'defaultValue' => null, 'description' => 'Aggregate values from that column, e.g "index.catalog.id" (optional, only if type is passed)'],
+					['name' => 'type', 'type' => Type::string(), 'defaultValue' => null, 'description' => 'Type of aggregation like "sum" or "avg" (default: null for count)'],
+					['name' => 'filter', 'type' => Type::string(), 'defaultValue' => '{}', 'description' => 'Filter conditions'],
+					['name' => 'sort', 'type' => Type::listOf( Type::string() ), 'defaultValue' => [], 'description' => 'Sort keys'],
+					['name' => 'limit', 'type' => Type::int(), 'defaultValue' => 10000, 'description' => 'Slice size'],
+				],
+				'resolve' => $this->aggregateItems( $domain ),
+			]
 		];
-
-		return $list;
 	}
 }
