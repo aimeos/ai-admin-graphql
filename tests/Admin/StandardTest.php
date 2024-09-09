3ad8cf3c67b27ef732adb6074ddddcf46959283b
Aimeos <aimeos@aimeos.org>
2024-09-09 18:52:53 +0200

    Adapt to simplified product manager

--- tests/Admin/StandardTest.php
+++ tests/Admin/StandardTest.php
@@ -144,11 +144,12 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 	{
 		$stub = $this->getMockBuilder( '\\Aimeos\\MShop\\Product\\Manager\\Standard' )
 			->setConstructorArgs( array( $this->context ) )
-			->onlyMethods( ['save'] )
+			->onlyMethods( ['save', 'getDomain'] )
 			->getMock();
 
 		$item = $stub->create( ['product.id' => 123, 'product.code' => 'test-graphql'] );
 		$stub->expects( $this->once() )->method( 'save' )->willReturn( [$item] );
+		$stub->method( 'getDomain' )->willReturn( 'product' );
 
 		\Aimeos\MShop::inject( '\\Aimeos\\MShop\\Product\\Manager\\Standard', $stub );
 
